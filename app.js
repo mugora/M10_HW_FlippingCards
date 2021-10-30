d6101ff031509435593fd292906fad151d664cc2
mugora <89990771+mugora@users.noreply.github.com>
2021-10-30 11:35:04 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+new Vue({
+    el: "#flashcards-app",
+    data: {
+        cards:[
+            {
+                question:"What year was IUPUI founded?",
+                answer:"1969",
+                category:"history",
+                flipped:false
+            },
+            {
+                question:"What is 2+2?",
+                answer:"Fish",
+                category:"math",
+                flipped:false
+            },{
+                question:"Is mayonnaise an instrument?",
+                answer:"No",
+                category:"science",
+                flipped:false
+            }
+        ]
+    },
+
+    methods:{
+        flipCard:function (index){
+            this.cards[index].flipped=true;
+        },
+
+        //adds items to the form
+        addItem: function (){
+            this.cards.push({question: this.form.question, answer: this.form.answer,
+                category: this.form.category, flipped: false})
+        },
+
+
+    form: {
+        question:"",
+        answer:"",
+        category:""
+    },
+
+
+        selectCategory: function (){
+
+        }
+
+
+    }
+})
